280f512c18a24611fdc4d9ec0920f6a02d952ec0
Tavarus <tavarus@Mac-mini.local>
2026-09-08 07:05:52 -0400

    feat(popup): turn off Labor Day popup notice

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts, ScrollRestoration } from "@tanstack/react-router";
 import { DiscountPopup } from "@/components/DiscountPopup";
-import { LaborDayPopup } from "@/components/LaborDayPopup";
 import { FomoPopup } from "@/components/FomoPopup";
 import { Toaster } from "@/components/ui/sonner";
 
@@ -101,7 +100,6 @@ function RootShell({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <LaborDayPopup />
         <DiscountPopup />
         <FomoPopup />
         <Toaster position="top-center" />
